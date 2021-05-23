9b05b3d215f9a07098a47edeff65750e3ead0ede
Vassilis <bkrith@hotmail.com>
2021-05-23 17:59:21 +0300

    update dependencies

--- lib/stream.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-import { Transform } from 'stream';
-import { MultiLevel } from './multilevel';
-export declare class TransformStream extends Transform {
-    private _chunk;
-    constructor(db?: any, events?: MultiLevel, opts?: any);
-    private getChunk;
-    private formatPacket;
-}
